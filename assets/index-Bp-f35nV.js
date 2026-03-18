b03acfb3ebc0e27cf17ab3320248fc82f8ac75dc
riyad168 <riyadkhan1684762@gmail.com>
2026-03-18 15:51:47 +0600

    Add files via upload

---
+++ assets/index-Bp-f35nV.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DOiDr4rh.js","assets/index-CV4Cquht.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-R54CrtPX.js";import{r as o}from"./index-CV4Cquht.js";const i=o("Browser",{web:()=>e(()=>import("./web-DOiDr4rh.js"),__vite__mapDeps([0,1])).then(r=>new r.BrowserWeb)});export{i as Browser};
---
+++ assets/index-Db0mhbMY.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CHwnw1Ie.js","assets/index-CV4Cquht.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-R54CrtPX.js";import{r as p}from"./index-CV4Cquht.js";const _=p("AppLauncher",{web:()=>e(()=>import("./web-CHwnw1Ie.js"),__vite__mapDeps([0,1])).then(r=>new r.AppLauncherWeb)});export{_ as AppLauncher};
